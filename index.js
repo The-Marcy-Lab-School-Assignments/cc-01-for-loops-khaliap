a9638e00c29b767c56aa48f81790977f4e28cc62
khalia <khaliaparris01@gmail.com>
2021-09-28 15:27:36 +0000

    count from one function

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ function countToFifty(){
 }
 
 //2
-function countFromOne(){
+function countFromOne(num){
+     for (let i = 1; i <= num; i++){
+        console.log(i)
+     }
   
 }
-
+countFromOne()
 //3
 function threeAndFiveInFifty(){
   
